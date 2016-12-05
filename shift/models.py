4c3e30a26e9b7a3f18539b2f0446aecc730a5081
manish <balyan05.manish@gmail.com>
2016-12-05 16:09:05 +0530

    updated view

--- shift/models.py
+++ shift/models.py
@@ -15,6 +15,13 @@ class Shifts(models.Model):
 class ShiftSlots(models.Model):
     shiftId = models.ForeignKey(Shifts, default=0, on_delete=models.CASCADE,)
     userId = models.ForeignKey(User, default=0,)
+    # open_slots= models.IntegerField(null=True, blank=True)
+    # @property
+    # def open_slot(self):
+    #     count_ss= ShiftSlots.objects.filter(shiftId= 6).count()
+    #     print (count_ss)
+    #     open_slot1= self.shiftId.capacity- count_ss
+    #     return open_slot1
 
     def __unicode__(self):
         return unicode(self.shiftId.id)
--- shift/views.py
+++ shift/views.py
@@ -13,11 +13,18 @@ def index(request):
         i.append(user_shift)
     user_shifts.update({'user_shift':i})
 
+    def open_slot(self):
+        count_ss = ShiftSlots.objects.filter(shiftId=6).count()
+        print (count_ss)
+        open_slot = self.shiftId.capacity - count_ss
+        return open_slot
 
 
 
-    # print user_shift_slot
-    # print user_shifts
+
+
+    print user_shift_slot
+    print user_shifts
 
     empty_slots = Shifts.objects.all()
 
