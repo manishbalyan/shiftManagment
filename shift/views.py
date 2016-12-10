183ff2838a1398fc3287b1e38e6da21b27a61f73
Manish <balyan05.manish@gmail.com>
2016-12-11 03:11:49 +0530

    final version for testing

--- shift/views.py
+++ shift/views.py
@@ -2,34 +2,29 @@ from django.shortcuts import render
 from .models import Shifts, ShiftSlots
 # Create your views here.
 
-
-
 def index(request):
     i=[]
+    j= []
     user_shifts=  {}
+    user_empty_slot = {}
     user_shift_slot = ShiftSlots.objects.filter(userId=request.user.id)
+    a= len(user_shift_slot)
     for shift in user_shift_slot:
         user_shift = Shifts.objects.get(id=shift.shiftId_id)
+        # print user_shift.capacity
+        empty_slot = user_shift.capacity- a
+        # print empty_slot
         i.append(user_shift)
+        j.append(empty_slot)
     user_shifts.update({'user_shift':i})
+    user_empty_slot.update({'empty_slots': j})
+    zipped = zip(user_shifts['user_shift'], user_empty_slot['empty_slots'])
 
-    def open_slot(self):
-        count_ss = ShiftSlots.objects.filter(shiftId=6).count()
-        print (count_ss)
-        open_slot = self.shiftId.capacity - count_ss
-        return open_slot
-
-
-
-
-
-    print user_shift_slot
-    print user_shifts
-
-    empty_slots = Shifts.objects.all()
+    # print (user_shifts,"shfts")
+    # print (user_empty_slot, "empty_slots")
 
     context_dict = {
-        'user_shifts': user_shifts
+        'zipped': zipped,
 
     }
     return render(request, 'home.html', context_dict)
